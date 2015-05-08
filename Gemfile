382ca29a37206acfaa1d2a116fb02e2077b77c18
MelanieJae <mtaurus575@gmail.com>
2015-05-08 00:43:03 +0000

    added new course page and form and linked up new and show pages

--- Gemfile
+++ Gemfile
@@ -34,6 +34,8 @@ gem 'autoprefixer-rails'
 
 gem 'devise'
 
+gem 'simple_form'
+
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
   gem 'sdoc', require: false
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Flixter::Application.routes.draw do
   devise_for :users
   root 'start_pages#index'
   resources :users, :only => :show
+  namespace :instructor do
+      resources :courses, :only => [:new, :create, :show]
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
