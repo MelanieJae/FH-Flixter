0f1ecb48cd4030a27ce12e4bc79c37e85e8d949a
MelanieJae <mtaurus575@gmail.com>
2015-05-24 20:59:20 +0000

    fixed new lesso form and added create lesson button

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -8,7 +8,7 @@ class Instructor::LessonsController < ApplicationController
 
 	def create
 		@lesson = @current_section.lessons.create(lesson_params)
-		redirect_to lesson_path
+		redirect_to course_path
 	end
 
 	private
