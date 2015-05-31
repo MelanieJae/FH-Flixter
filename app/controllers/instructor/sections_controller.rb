6915add43815f0dae7cc5160f77e66d2c6187d8b
MelanieJae <mtaurus575@gmail.com>
2015-05-31 20:16:24 +0000

    moved new section form into modal, removed section#new page and action

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,11 +1,7 @@
 class Instructor::SectionsController < ApplicationController
 	before_action :authenticate_user!
-	before_action :require_authorized_for_current_course
+	before_action :require_authorized_for_current_course, :only => [:create]
 
-
-	def new
-		@section = Section.new
-	end
 
 	def create
 		@section = current_course.sections.create(section_params)
