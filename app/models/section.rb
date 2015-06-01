39e3f398c1e887f7a98b1805a5e7bc91c80cc800
MelanieJae <mtaurus575@gmail.com>
2015-06-01 22:56:17 +0000

    added to footer

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,4 +1,9 @@
 class Section < ActiveRecord::Base
 	belongs_to :course
 	has_many :lessons
+
+	def next_section
+		section = course.sections.where("row_order > ?", self.row_order).rank(:row_order).first
+		return section
+	end
 end
